acf5d923823b51e37466d2305352a96652918efc
Magnus <magnus.longva@bouvet.no>
2021-09-20 11:18:50 +0200

    removed try catch

--- src/main/java/no/sikkerhetshull/xxe/XmlController.java
+++ src/main/java/no/sikkerhetshull/xxe/XmlController.java
@@ -5,6 +5,10 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.xml.sax.SAXException;
+
+import javax.xml.transform.TransformerException;
+import java.io.IOException;
 
 @RestController
 public class XmlController {
@@ -13,11 +17,11 @@ public class XmlController {
     private XmlParserService xmlParserService;
 
     @PostMapping(value = "/hello", consumes = {MediaType.APPLICATION_XML_VALUE, MediaType.TEXT_XML_VALUE})
-    public String postData(@RequestBody String xmlString) {
-        try {
+    public String postData(@RequestBody String xmlString) throws TransformerException, SAXException, IOException {
+//        try {
             return xmlParserService.parse(xmlString);
-        } catch (Exception e) {
-            return e.getStackTrace().toString();
-        }
+//        } //catch (Exception e) {
+//            return e.getStackTrace().toString();
+//        }
     }
 }

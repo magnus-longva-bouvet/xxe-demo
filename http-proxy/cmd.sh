a5de62937614b77f6a28f7912dedf9982f76a2ec
Magnus Longva (IT MSS LID) <magnus@longva.email>
2022-10-19 11:38:40 +0200

    changed proxy image. noproxy env tuned

--- http-proxy/cmd.sh
+++ http-proxy/cmd.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 echo ${CA_KEY} | base64 -d > /config/ca.key.pem
 /app/inkfish -cacert /config/ca.pem -cakey /config/ca.key.pem -metadata none -config /config
--- xxe-demo/cmd.sh
+++ xxe-demo/cmd.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-java -Dhttp.proxyHost=http-proxy -Dhttp.proxyPort=${proxyport} -Dhttps.proxyHost=http-proxy -Dhttps.proxyPort=${proxyport} -jar /app.jar
+if [ -n $use_proxy ]; then
+    java -Dhttp.proxyHost=http-proxy -Dhttp.proxyPort=${proxyport} -Dhttps.proxyHost=http-proxy -Dhttps.proxyPort=${proxyport} -jar /app.jar
+else
+    java -jar /app.jar
+fi

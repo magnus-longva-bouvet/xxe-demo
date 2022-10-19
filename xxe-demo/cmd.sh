56fb48460b36f6c343442c8d89ae9ee92a0e545b
Magnus Longva (IT MSS LID) <magnus@longva.email>
2022-10-19 11:49:37 +0200

    no evildtd in noproxy. fixed use_proxy env var test

--- xxe-demo/cmd.sh
+++ xxe-demo/cmd.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if [ -n $use_proxy ]; then
+if [ -n "$use_proxy" ]; then
     java -Dhttp.proxyHost=http-proxy -Dhttp.proxyPort=${proxyport} -Dhttps.proxyHost=http-proxy -Dhttps.proxyPort=${proxyport} -jar /app.jar
 else
     java -jar /app.jar
